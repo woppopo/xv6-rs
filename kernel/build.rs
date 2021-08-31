a2ec603a5912f5599cd12be60c62d0d631bf657c
woppopo <woppopo@protonmail.com>
2021-08-31 18:30:41 +0900

    Make able to compile kernel (But it fails to boot)

--- kernel/build.rs
+++ kernel/build.rs
@@ -1,28 +1,34 @@
-fn main() {
-    println!(
-        "cargo:rustc-link-search={}",
-        std::env::var("OUT_DIR").unwrap()
-    );
+use std::path::PathBuf;
+use std::process::Command;
 
+const CC: &'static str = "gcc";
+const LD: &'static str = "ld";
+const OBJCOPY: &'static str = "objcopy";
+
+const CFLAGS: &[&'static str] = &[
+    "-fno-pic",
+    "-static",
+    "-fno-builtin",
+    "-fno-strict-aliasing",
+    "-O2",
+    "-Wall",
+    //"-MD",
+    "-ggdb",
+    "-m32",
+    //"-Werror",
+    "-fno-omit-frame-pointer",
+    "-fno-stack-protector",
+];
+
+const LDFLAGS: &[&'static str] = &["-m", "elf_i386"];
+
+fn main() {
     let mut build = cc::Build::new();
     {
-        build
-            //.warnings(true)
-            .flag("-fno-pic")
-            .flag("-fno-pie")
-            .flag("-no-pie")
-            .flag("-static")
-            .flag("-fno-builtin")
-            .flag("-fno-strict-aliasing")
-            .flag("-O2")
-            .flag("-Wall")
-            //.flag("-MD")
-            .flag("-ggdb")
-            .flag("-m32")
-            //.flag("-Werror")
-            .flag("-fno-omit-frame-pointer")
-            .flag("-fno-stack-protector")
-            .include("c/");
+        build.include("c/");
+        for flag in CFLAGS {
+            build.flag(flag);
+        }
     }
 
     let c_files = [
@@ -60,7 +66,7 @@ fn main() {
         println!("cargo:rustc-link-lib=static={}", file);
     }
 
-    let asm_files = ["swtch", "trapasm", "vectors"];
+    let asm_files = ["entry", "swtch", "trapasm", "vectors"];
     for file in asm_files {
         build
             .clone()
@@ -68,4 +74,56 @@ fn main() {
             .compile(&format!("lib{}.a", file));
         println!("cargo:rustc-link-lib=static={}", file);
     }
+
+    let out_path = PathBuf::from(std::env::var("OUT_DIR").unwrap());
+    initcode(&out_path);
+    entryother(&out_path);
+}
+
+fn initcode(out_path: &PathBuf) {
+    Command::new(CC)
+        .args(CFLAGS)
+        .args(&["-nostdinc", "-I.", "-c", "c/initcode.S", "-o"])
+        .arg(out_path.join("initcode.o"))
+        .status()
+        .unwrap();
+
+    Command::new(LD)
+        .args(LDFLAGS)
+        .args(&["-N", "-e", "start", "-Ttext", "0", "-o"])
+        .arg(out_path.join("initcode.out"))
+        .arg(out_path.join("initcode.o"))
+        .status()
+        .unwrap();
+
+    Command::new(OBJCOPY)
+        .args(&["-S", "-O", "binary"])
+        .arg(out_path.join("initcode.out"))
+        .arg(out_path.join("initcode"))
+        .status()
+        .unwrap();
+}
+
+fn entryother(out_path: &PathBuf) {
+    Command::new(CC)
+        .args(CFLAGS)
+        .args(&["-fno-pic", "-nostdinc", "-I.", "-c", "c/entryother.S", "-o"])
+        .arg(out_path.join("entryother.o"))
+        .status()
+        .unwrap();
+
+    Command::new(LD)
+        .args(LDFLAGS)
+        .args(&["-N", "-e", "start", "-Ttext", "0x7000", "-o"])
+        .arg(out_path.join("entryother.out"))
+        .arg(out_path.join("entryother.o"))
+        .status()
+        .unwrap();
+
+    Command::new(OBJCOPY)
+        .args(&["-S", "-O", "binary", "-j", ".text"])
+        .arg(out_path.join("entryother.out"))
+        .arg(out_path.join("entryother"))
+        .status()
+        .unwrap();
 }
--- kernel/src/main.rs
+++ kernel/src/main.rs
@@ -6,14 +6,31 @@ fn panic(_info: &core::panic::PanicInfo) -> ! {
     loop {}
 }
 
-#[link(name = "main")]
-extern "C" {
-    fn main();
-}
+#[repr(transparent)]
+struct StaticPointer<T>(*const T);
 
+unsafe impl<T> Sync for StaticPointer<T> {}
+
+#[used]
 #[no_mangle]
-extern "C" fn test() {
-    unsafe {
-        main();
-    }
-}
+static INITCODE: &'static [u8] = include_bytes!(concat!(env!("OUT_DIR"), "/initcode"));
+
+#[used]
+#[no_mangle]
+static _binary_initcode_start: StaticPointer<u8> = StaticPointer(INITCODE.as_ptr());
+
+#[used]
+#[no_mangle]
+static _binary_initcode_size: usize = INITCODE.len();
+
+#[used]
+#[no_mangle]
+static ENTRYOTHER: &'static [u8] = include_bytes!(concat!(env!("OUT_DIR"), "/entryother"));
+
+#[used]
+#[no_mangle]
+static _binary_entryother_start: StaticPointer<u8> = StaticPointer(ENTRYOTHER.as_ptr());
+
+#[used]
+#[no_mangle]
+static _binary_entryother_size: usize = ENTRYOTHER.len();
