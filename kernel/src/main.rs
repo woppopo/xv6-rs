261ecc5aaac1b607c8812ea685985f2e2b82a825
woppopo <woppopo@protonmail.com>
2021-08-31 20:56:29 +0900

    Move main

--- kernel/src/main.rs
+++ kernel/src/main.rs
@@ -37,3 +37,61 @@ static _binary_entryother_start: StaticPointer<u8> = StaticPointer(ENTRYOTHER.as
 #[used]
 #[no_mangle]
 static _binary_entryother_size: usize = ENTRYOTHER.len();
+
+// Bootstrap processor starts running C code here.
+// Allocate a real stack and switch to it, first
+// doing some setup required for memory allocator to work.
+#[no_mangle]
+unsafe extern "C" fn main() {
+    pub const PHYSTOP: usize = 0xE000000; // Top physical memory
+
+    extern "C" {
+        fn end(); // first address after kernel loaded from ELF file
+        fn kinit1(vstart: *const u8, vend: *const u8);
+        fn kvmalloc();
+        fn mpinit();
+        fn lapicinit();
+        fn seginit();
+        fn picinit();
+        fn ioapicinit();
+        fn consoleinit();
+        fn uartinit();
+        fn pinit();
+        fn tvinit();
+        fn binit();
+        fn fileinit();
+        fn ideinit();
+        fn startothers();
+        fn kinit2(vstart: *const u8, vend: *const u8);
+        fn userinit();
+        fn mpmain();
+    }
+
+    pub const KERNBASE: usize = 0x80000000; // First kernel virtual address
+    fn p2v(paddr: usize) -> usize {
+        paddr + KERNBASE
+    }
+
+    fn v2p(vaddr: usize) -> usize {
+        vaddr - KERNBASE
+    }
+
+    kinit1(end as _, p2v(4 * 1024 * 1024) as _); // phys page allocator
+    kvmalloc(); // kernel page table
+    mpinit(); // detect other processors
+    lapicinit(); // interrupt controller
+    seginit(); // segment descriptors
+    picinit(); // disable pic
+    ioapicinit(); // another interrupt controller
+    consoleinit(); // console hardware
+    uartinit(); // serial port
+    pinit(); // process table
+    tvinit(); // trap vectors
+    binit(); // buffer cache
+    fileinit(); // file table
+    ideinit(); // disk
+    startothers(); // start other processors
+    kinit2(p2v(4 * 1024 * 1024) as _, p2v(PHYSTOP) as _); // must come after startothers()
+    userinit(); // first user process
+    mpmain(); // finish this processor's setup
+}
