b52542f9dcf41d20c5d6f44c487e0e149164107d
woppopo <woppopo@protonmail.com>
2021-08-31 23:09:01 +0900

    Remove old code

--- kernel/src/trapasm.rs
+++ kernel/src/trapasm.rs
@@ -1,66 +1,5 @@
 const SEG_KDATA: u32 = 2;
 
-#[repr(C)]
-pub struct TrapFrame {
-    // registers as pushed by pusha
-    pub edi: u32,
-    pub esi: u32,
-    pub ebp: u32,
-    pub oesp: u32, // useless & ignored
-    pub ebx: u32,
-    pub edx: u32,
-    pub ecx: u32,
-    pub eax: u32,
-
-    // rest of trap frame
-    pub gs: u32,
-    pub fs: u32,
-    pub es: u16,
-    pub ds: u16,
-
-    pub trapno: u32,
-
-    // below here defined by x86 hardware
-    pub err: u32,
-    pub eip: u32,
-    pub cs: u16,
-    pub padding5: u16,
-    pub eflags: u32,
-
-    // below here only when crossing rings, such as from user to kernel
-    pub esp: u32,
-    pub ss: u16,
-    pub padding6: u16,
-}
-
-impl TrapFrame {
-    pub fn zero() -> Self {
-        Self {
-            edi: 0,
-            esi: 0,
-            ebp: 0,
-            oesp: 0,
-            ebx: 0,
-            edx: 0,
-            ecx: 0,
-            eax: 0,
-            gs: 0,
-            fs: 0,
-            es: 0,
-            ds: 0,
-            trapno: 0,
-            err: 0,
-            eip: 0,
-            cs: 0,
-            padding5: 0,
-            eflags: 0,
-            esp: 0,
-            ss: 0,
-            padding6: 0,
-        }
-    }
-}
-
 global_asm!(
     r#"
     # vectors.S sends all traps here.
@@ -97,7 +36,3 @@ global_asm!(
     ds = const (SEG_KDATA << 3),
     options(att_syntax)
 );
-
-extern "C" {
-    pub fn trapret();
-}
